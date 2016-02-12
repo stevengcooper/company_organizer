ef19a853104897327895fb222bb4a02218d560ea
Steven Cooper <stevengcooper76@gmail.com>
2016-02-12 11:42:40 -0500

    Employee performance marks unsatisfactory. Updates key word argument format.

--- employee.rb
+++ employee.rb
@@ -2,19 +2,31 @@ require 'byebug'
 require './reviews.rb'
 
 class Employee
-  attr_reader :name, :email, :phone_number, :salary, :department, :reviews
+  attr_reader :name, :email, :phone, :salary, :department
 
-  def initialize(name, email, phone_number, salary, department = nil)
+  attr_accessor :review
+
+  def initialize(name: nil, email: nil, phone: nil, salary: nil, department: nil, review: nil, employee_performance: nil)
     @name = name
     @email = email
-    @phone_number = phone_number
+    @phone = phone
     @salary = salary
     @department = department
-    @reviews = []
+    @employee_performance = employee_performance
+    @review = review
   end
 
   def access_review(name)
-    @reviews.select {|n| n.match(/\b#{name}\b/i)}
+    # @reviews.select {|n| (n.match(/\b#{name}\b/i)) ? (n = name.review) : false}
   end
+
+  def employee_performance
+    @employee_performance = "unsatisfactory"
+  end
+
+
+
+  # def determine_performance(access_review(name))
+  # end
 
 end
--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'byebug'
 
 #Note: This line is going to fail first.
 require './employee_reviews.rb'
@@ -15,34 +16,34 @@ class EmployeeReviews < Minitest::Test
   end
 
   def test_department_has_a_name
-    assert Department.new("Accounting")
+    assert Department.new( "Accounting")
   end
 
   def test_employee_class_exits
-    assert Employee.new("Bill Smith", "bill@yahoo.com", "919.343.4567", 60000)
+    aa = Employee.new(name: "Bill Smith",email:   "bill@yahoo.com", phone: "919.343.4567", salary: 60000)
   end
 
   def test_add_employee_to_department
-    accounting = Department.new("Accounting")
-    new_worker = Employee.new("Bill Smith", "bill@yahoo.com", "919.343.4567", 60000)
-    assert accounting.add_employee(new_worker)
-    assert_equal new_worker, accounting.staff[0]
+    accounting = Department.new( "Accounting")
+    a = Employee.new(name: "Bill Smith",email:   "bill@yahoo.com", phone: "919.343.4567", salary: 60000)
+    assert accounting.add_employee(a)
+    assert_equal a, accounting.staff[0]
   end
 
   def test_get_employee_name
-    a = Employee.new("Bill Smith", "bill@yahoo.com", "919.343.4567", 60000)
+    a = Employee.new(name: "Bill Smith",email:   "bill@yahoo.com", phone: "919.343.4567", salary: 60000)
     assert_equal "Bill Smith", a.name
     assert_equal  60000, a.salary
   end
 
   def test_get_a_department_name
-    accounting = Department.new("Accounting")
+    accounting = Department.new( "Accounting")
     assert_equal "Accounting", accounting.name
   end
 
   def test_get_total_salaries_for_employees_in_department
-    a = Employee.new("Bill Smith", "bill@yahoo.com", "919.343.4567", 60000)
-    b = Employee.new("Yvonne Yo", "yvonne@gmail.com", "919.834.8765", 60000)
+    a = Employee.new(name: "Bill Smith",email:   "bill@yahoo.com", phone: "919.343.4567", salary: 60000)
+    b = Employee.new(name: "Yvonne Yo",email: "yvonne@gmail.com", phone: "919.834.8765", salary: 60000)
     accounting = Department.new("Accounting")
     assert accounting.add_employee(a)
     assert accounting.add_employee(b)
@@ -51,11 +52,24 @@ class EmployeeReviews < Minitest::Test
   end
 
   def test_add_employee_review_text_to_employee
-    a = Employee.new("Bill Smith", "bill@yahoo.com", "919.343.4567", 60000)
-    b = Employee.new("Yvonne Yo", "yvonne@gmail.com", "919.834.8765", 49000)
-    assert a.access_review(a.name)
-    assert b.access_review(b.name)
+    b = Employee.new(name: "Yvonne Yo",email: "yvonne@gmail.com", phone: "919.834.8765", salary: 60000,review: "Thus far, there have been two concerns over Yvonne's performance, and both have been discussed with her in internal meetings.  First, in some cases, Yvonne takes longer to complete tasks than would normally be expected.  This most commonly manifests during development on existing applications, but can sometimes occur during development on new projects, often during tasks shared with Andrew.  In order to accommodate for these preferences, Yvonne has been putting more time into fewer projects, which has gone well.")
+    assert b.review
+    assert_equal b.review, "Thus far, there have been two concerns over Yvonne's performance, and both have been discussed with her in internal meetings.  First, in some cases, Yvonne takes longer to complete tasks than would normally be expected.  This most commonly manifests during development on existing applications, but can sometimes occur during development on new projects, often during tasks shared with Andrew.  In order to accommodate for these preferences, Yvonne has been putting more time into fewer projects, which has gone well."
   end
+
+  def test_employee_performance_is_satisfactory_or_not
+    b = Employee.new(name: "Yvonne Yo", employee_performance: "unsatisfactory")
+    assert b.employee_performance
+    assert_equal b.employee_performance, "unsatisfactory"
+  end
+  # def test_determine_employee_performance
+  #   b = Employee.new("Yvonne Yo", "yvonne@gmail.com", "919.834.8765", 49000)
+  #   assert employee.determine_performance(access_review(b.name))
+  #   assert_equal employee.determine_performance(access_review(b.name)), false
+  # end
+
+
+
 
 
 
